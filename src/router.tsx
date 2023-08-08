1dfba4c7e18ce2e9e6a4ef613de7b8f2065a2ace
André L O Júnior <atipicode.ec@gmail.com>
2023-08-07 22:47:48 -0300

    mudando nome da página inicial de home para main

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
 // importing element pages
-import Home from "./pages/Home";
+import Home from "./pages/Main";
 import News from "./pages/News";
 import Auth from "./pages/Auth";
 import CreateNews from "./pages/CreateNews";
