e2c398a2bbbd81e2301b318d49fe050086c85e0b
André L O Júnior <atipicode.ec@gmail.com>
2023-08-07 23:38:22 -0300

    adição da primary color nos presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F2A900",
+      }
+    },
   },
   plugins: [],
 }
