5d422bcd20e21cc78fd392cdc3d44b7add4a6e39
André L O Júnior <atipicode.ec@gmail.com>
2023-08-08 22:46:10 -0300

    auth alternative headers and button component

--- src/pages/Main/components/header.tsx
+++ src/pages/Main/components/header.tsx
@@ -1,7 +1,18 @@
 import logo from '../../../assets/tnt_logo.png';
-import { MagnifyingGlass } from '@phosphor-icons/react'
+import { MagnifyingGlass, User, SignOut } from '@phosphor-icons/react'
+import { useNavigate } from 'react-router';
+import { Button } from '../../../components/button';
 
-function HeaderMain() {
+interface HeaderMainProps {
+    user?: {
+        email: string;
+        nome: string;
+        admin: boolean;
+    };
+}
+
+function HeaderMain({ user }: HeaderMainProps) {
+    const navigate = useNavigate()
 
     return (
         <header className='flex justify-between bg-zinc-900 px-10 py-4 border-b border-b-black/50'>
@@ -10,14 +21,27 @@ function HeaderMain() {
                 <span className='text-white text-lg'>Todas as notícias titânicas estão aqui!</span>
             </div>
 
-            <div className='flex items-center gap-5'>
+            <div className='flex items-center gap-8'>
                 <div className='relative'>
                     <MagnifyingGlass size={18} className='absolute top-1/2 -translate-y-1/2 left-2 text-primary' />
                     <input type="text" placeholder='Pesquisar notícia...' className='w-80 pl-9 py-2 bg-zinc-800 rounded-md text-white' />
                 </div>
-                <button type="button" className='px-12 py-2 bg-primary rounded-md'>
-                    <span className='text-zinc-900'>ENTRAR</span>
-                </button>
+                {
+                    !user ?
+                    <Button text='ENTRAR' action={() => navigate('/auth')} />
+                    :
+                    <div className='flex items-center gap-8'>
+                        {user.admin && <Button text='Nova notícia' action={() => navigate('/create-news')} />}
+                        <div className='flex gap-2'>
+                            <User size={24} weight='fill' className='text-zinc-950' />
+                            <p className='text-white'>Olá, <strong>{user.nome.split(' ')[0]}</strong>!</p>
+                        </div>
+                        <button type='button' title='Sair' onClick={() => {}}>
+                            <SignOut size={24} className='text-red-600' />
+                        </button>
+                    </div>
+                }
+                
             </div>
         </header>
     )
