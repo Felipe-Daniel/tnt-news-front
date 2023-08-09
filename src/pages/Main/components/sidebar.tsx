26566b848835fa88c2aa72db56b9acfe5155b341
André L O Júnior <atipicode.ec@gmail.com>
2023-08-08 23:00:14 -0300

    esboço da sidebar

---
+++ src/pages/Main/components/sidebar.tsx
@@ -0,0 +1,8 @@
+export function Sidebar() {
+
+    return (
+        <div className="w-2/12 bg-zinc-800">
+            
+        </div>
+    )
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,17 @@
 import HeaderMain from './components/header'
+import { Sidebar } from './components/sidebar'
 
 function Main() {
 
   return (
-    <div className='bg-zinc-900 h-screen'>
+    <div className='bg-zinc-900 h-screen flex flex-col'>
       <HeaderMain />
+      <div className='flex h-full'>
+        <Sidebar />
+        <section className='w-10/12'>
+          
+        </section>
+      </div>
     </div>
   )
 }
